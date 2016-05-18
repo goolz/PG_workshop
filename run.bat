c06795527ca1a10f05563bf4363cff766815f5c8
Keyvan Montazeri <k1m@ipcgroup.ir>
2016-05-18 21:44:26 +0430

    get data ajax

--- run.bat
+++ run.bat
@@ -1,3 +1,14 @@
 @echo off
 
+echo #################################
+echo Remove Platforms
+echo #################################
+call cordova platforms remove browser
+echo browser has been removed
+timeout 1
+echo #################################
+echo Add Platforms
+echo #################################
+call cordova platforms add browser
+echo browser has been added
 call cordova run browser
